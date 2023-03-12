a91720d8eb29a4f426f1987ee4479d5785c1ed44
Rohit hcr2 <106584386+coderrohit2927@users.noreply.github.com>
2023-03-12 09:42:37 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
+import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
+import { About } from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setdarkMode] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+     <Navbar title="ComingSlow"/>
+     <div className='container'>
+     {/* <TextForm heading='Enter the text'/> */}
+     <About/>
+     </div>
+    
+    </>
   );
 }
 
